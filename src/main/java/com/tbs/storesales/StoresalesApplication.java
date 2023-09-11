8b0988d6531d652597af612c3f650ce5db8ebff6
Heber Cordova <u201924464@upc.edu.pe>
2023-09-11 14:52:42 -0500

    feat: project base
    added lombok, devtools, web, jpa, mysql drive and configure database

---
+++ src/main/java/com/tbs/storesales/StoresalesApplication.java
@@ -0,0 +1,13 @@
+package com.tbs.storesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoresalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoresalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbs/storesales/StoresalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbs.storesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoresalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
